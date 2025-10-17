457e96163c29901420ccdc59569a47b594ad3776
Leu Pytsko <pytskoleu123@gmail.com>
2025-10-17 23:47:43 +0300

    Работает, но не влазит

--- Algorithm/Additional tasks/Task20(Палиндромы)/Task20(Палиндромы)/Task20(Палиндромы).cpp
+++ Algorithm/Additional tasks/Task20(Палиндромы)/Task20(Палиндромы)/Task20(Палиндромы).cpp
@@ -3,72 +3,92 @@
 // читаемый слева-направо и справа-налево. Необходимо найти палиндром максимальной длины, который можно получить из
 // исходной строки вычеркиванием символов (менять порядок символов нельзя). Так как ответов может быть несколько, то найти
 // минимальный и максимальный лексикографически (как в словаре).
-/* Будем искать LCS строки s и обратной ей строки t это и будет палиндром. В dp[i][j] лежит длина LCS(s,t) начиная с i 
-в s и j в t, т.е dp[0][0] и будет размер самого длинного палиндрома и далее будем восстанавливать наш палиндром длины
-dp[0][0] добавляя в результат букву, если после этого ещё можно собрать палиндром максимальной длины*/
-/* При маленьких строках программа работает корректно и проверки dp[i1 + 1][j1 + 1] >= rem - 1(*) достаточно, однако эта 
-гарантирует только то, что наш результат будет нужной длины, но не гарантирует того, что он будет палиндромом и таким
-образом мы можем добавить не ту букву(ищем первую удовлетворяющую условию (*) но это не всегда правильно),ведь после этого
-может уже не выйти собрать палиндром.В больших строках много повторяющихся букв и там этот алгоритм даёт сбой у нас
-получается не палиндром.
-Пример: gxlnxvsjxzdsvxcpfaeuwcffvvkyojetfdcsuyfihuirdwfyfgpwnrkadxuqxwxhwpbwnmebmolmzsdjucxtpibtbhhtlvvjgtcjdngtirzbshzufhbmgrsveglylyqtnemkvupvydendkythmvanliblqfdxlabajardccjikobbqzsajuqtpdtxmzuuwhpmwceodtpbfozarnkhaoxevsvrezqwnzwiggcvjmlpjlrkgenwmreswlcnxxlgbl
-glxxcewkjcgwnqxhnbomuxtpbbjcdrbfblnmkdedkmnlbfbrdcjbbptxmwhwdakrnwgcjkwecxxlg
-glxxsewkjciwwrxhnbomzxtptbjcdrbfllynevuvkmnllfbrdcjbtptxzwpwdarnwwicjkwesxxlg
-Правильный ответ:
-glxxcewkjcgwnqxhnbomuxtpbbjcdrbfblnmkdedkmnlbfbrdcjbbptxumobnhxqnwgcjkwecxxlg
-glxxsewkjciwwrxhnbomzxtptbjcdrbfllynevuvenyllfbrdcjbtptxzmobnhxrwwicjkwesxxlg
-*/
+/* Будем хранить в dp[l][r] размер максимального палиндрома в подстроке s[l...r], тогда dp[0][n -1] будет размер
+максимального палиндрома. В next[i][c] лежит первая позиция >=i для каждой буквы , а в prev[i + 1][c] последняя
+позиция <= i-1 для каждой буквы*/
+/* Оценка сложности: O(n^2) + 2*O(26*n) + O(26*n) ~ O(n^2) (Построение dp + построение next and prev + реконструкция
+палиндрома(need не больше n и букв 26.
+Память: O(n^2) + O(52*n^2) + O(k*n) ~ O(n^2) (dp + next + prev + (string s and other strings to store results) При
+n = 10000(int занимает 4 байта) память ~ 400 МБ(ограничение 256 МБ) */
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-string buildPal(string& s, string& t, vector<vector<int>>& dp, vector<vector<int>>& nextS,
-    vector<vector<int>>& nextT, bool min) {
+string buildPal(string& s,vector<vector<int>>& dp, vector<vector<int>>& nextS,
+    vector<vector<int>>& prevS, bool min) {
     int n = s.size();
-    int i = 0, j = 0;
-    int rem = dp[0][0];
-    string res;
-    res.reserve(rem);
-    while (rem > 0) {
+    int L = 0, R = n-1;
+    int need = dp[0][n-1];
+    vector<char> leftPart;
+    string mid;
+    while (need > 0 && L <= R) {
         bool found = false;
+        if (need == 1) {
+            if (min) {
+                for (int c = 0; c < 26; c++) {
+                    int pos = nextS[L][c];
+                    if (pos != -1 && pos <= R) {
+                        mid.push_back('a' + c); need = 0; break;
+                    }
+                }
+            }
+            else {
+                for (int c = 25; c >= 0; c--) {
+                    int pos = nextS[L][c];
+                    if (pos != -1 && pos <= R) {
+                        mid.push_back('a' + c); need = 0; break;
+                    }
+                }
+            }
+            break;
+        }
         if (min) {
             for (int c = 0; c < 26; c++) {
-                int i1 = (i <= n ? nextS[i][c] : -1);
-                int j1 = (j <= n ? nextT[j][c] : -1);
+                int i1 = nextS[L][c];
+                int j1 = prevS[R + 1][c];
                 if (i1 == -1 || j1 == -1) continue;
-                if (dp[i1 + 1][j1 + 1] >= rem - 1) {
-                    res.push_back(char('a' + c));
-                    i = i1 + 1;
-                    j = j1 + 1;
-                    rem--;
+                if (i1 >= j1) continue;
+                int inner = (i1 + 1 <= j1 - 1 ? dp[i1 + 1][j1 - 1] : 0);
+                if (inner >= need - 2) {
+                    leftPart.push_back('a' + c);
+                    L = i1 + 1;
+                    R = j1 - 1;
+                    need -= 2;
                     found = true;
                     break;
                 }
             }
-        }
-        else {
+        }else {
             for (int c = 25; c >= 0; c--) {
-                int i1 = (i <= n ? nextS[i][c] : -1);
-                int j1 = (j <= n ? nextT[j][c] : -1);
+                int i1 = nextS[L][c];
+                int j1 = prevS[R + 1][c];
                 if (i1 == -1 || j1 == -1) continue;
-                if (i1 >= n || j1 >= n) continue;
-                if (dp[i1 + 1][j1 + 1] >= rem - 1) {
-                    res.push_back(char('a' + c));
-                    i = i1 + 1;
-                    j = j1 + 1;
-                    rem--;
+                if (i1 >= j1) continue;
+                int inner = (i1 + 1 <= j1 - 1 ? dp[i1 + 1][j1 - 1] : 0);
+                if (inner >= need - 2) {
+                    leftPart.push_back('a' + c);
+                    L = i1 + 1;
+                    R = j1 - 1;
+                    need -= 2;
                     found = true;
                     break;
                 }
             }
         }
         if (!found) {
-            if (i < s.size() && dp[i + 1][j] >= dp[i][j + 1]) i++;
-            else j++;
+            int valL = (L + 1 <= R ? dp[L + 1][R] : 0);
+            int valR = (L <= R - 1 ? dp[L][R - 1] : 0);
+            if (valL >= valR) L++;
+            else R--;
         }
     }
+    string res;
+    res.reserve(leftPart.size() * 2 + mid.size());
+    for (char c : leftPart) res.push_back(c);
+    if (!mid.empty()) res += mid;
+    for (auto it = leftPart.rbegin(); it != leftPart.rend(); it++) res.push_back(*it);
     return res;
 }
 
@@ -77,30 +97,33 @@ int main()
     string s;
     cin >> s;
     int n = s.size();
-    string t = s;
-    reverse(t.begin(), t.end());
-    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-    for (int i = n - 1; i >= 0; i--) {
-        for (int j = n - 1; j >= 0; j--) {
-            if (s[i] == t[j]) {
-                dp[i][j] = 1 + dp[i + 1][j + 1];
+    vector<vector<int>> dp(n , vector<int>(n, 0));
+    for (int i = 0; i < n; i++) dp[i][i] = 1;
+    for (int len = 2; len <= n; len++) {
+        for (int l = 0;l + len - 1 < n;l++) {
+            int r = l + len - 1;
+            if (s[l] == s[r]) {
+                if (l + 1 <= r - 1) dp[l][r] = dp[l + 1][r - 1] + 2;
+                else dp[l][r] = 2;
             }
             else {
-                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+                dp[l][r] = max(dp[l + 1][r], dp[l][r - 1]);
             }
         }
     }
 
-    vector<vector<int>> nextS(n + 1, vector<int>(26, -1)), nextT(n + 1, vector<int>(26, -1));
+    vector<vector<int>> next(n + 1, vector<int>(26, -1)), prev(n + 1, vector<int>(26, -1));
     for (int i = n - 1;i >= 0;i--) {
-        for (int c = 0; c < 26; c++) nextS[i][c] = nextS[i + 1][c];
-        nextS[i][s[i] - 'a'] = i;
-        for (int c = 0; c < 26; c++) nextT[i][c] = nextT[i + 1][c];
-        nextT[i][t[i] - 'a'] = i;
+        for (int c = 0; c < 26; c++) next[i][c] = next[i + 1][c];
+        next[i][s[i] - 'a'] = i;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int c = 0; c < 26; c++) prev[i + 1][c] = prev[i][c];
+        prev[i + 1][s[i] - 'a'] = i;
     }
 
-    string minPal = buildPal(s, t, dp, nextS, nextT, true);
-    string maxPal = buildPal(s, t, dp, nextS, nextT, false);
+    string minPal = buildPal(s, dp, next, prev, true);
+    string maxPal = buildPal(s, dp, next, prev, false);
 
     cout << minPal << "\n" << maxPal;
 }
