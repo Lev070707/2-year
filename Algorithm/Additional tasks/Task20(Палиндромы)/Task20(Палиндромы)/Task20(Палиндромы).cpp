5fed4cacce2c06eb624d12a6d8bf0795a5648dab
Leu Pytsko <pytskoleu123@gmail.com>
2025-10-18 00:00:11 +0300

    Работает и влазит

--- Algorithm/Additional tasks/Task20(Палиндромы)/Task20(Палиндромы)/Task20(Палиндромы).cpp
+++ Algorithm/Additional tasks/Task20(Палиндромы)/Task20(Палиндромы)/Task20(Палиндромы).cpp
@@ -9,34 +9,35 @@
 /* Оценка сложности: O(n^2) + 2*O(26*n) + O(26*n) ~ O(n^2) (Построение dp + построение next and prev + реконструкция
 палиндрома(need не больше n и букв 26.
 Память: O(n^2) + O(52*n^2) + O(k*n) ~ O(n^2) (dp + next + prev + (string s and other strings to store results) При
-n = 10000(int занимает 4 байта) память ~ 400 МБ(ограничение 256 МБ) */
+n = 10000(short занимает 4 байта) память ~ 400 МБ(ограничение 256 МБ) 
+upd: все int заменеы на short, теперь всё влезло(short занимает 2 байта)*/
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-string buildPal(string& s,vector<vector<int>>& dp, vector<vector<int>>& nextS,
-    vector<vector<int>>& prevS, bool min) {
-    int n = s.size();
-    int L = 0, R = n-1;
-    int need = dp[0][n-1];
+string buildPal(string& s,vector<vector<short>>& dp, vector<vector<short>>& nextS,
+    vector<vector<short>>& prevS, bool min) {
+    short n = s.size();
+    short L = 0, R = n-1;
+    short need = dp[0][n-1];
     vector<char> leftPart;
     string mid;
     while (need > 0 && L <= R) {
         bool found = false;
         if (need == 1) {
             if (min) {
-                for (int c = 0; c < 26; c++) {
-                    int pos = nextS[L][c];
+                for (short c = 0; c < 26; c++) {
+                    short pos = nextS[L][c];
                     if (pos != -1 && pos <= R) {
                         mid.push_back('a' + c); need = 0; break;
                     }
                 }
             }
             else {
-                for (int c = 25; c >= 0; c--) {
-                    int pos = nextS[L][c];
+                for (short c = 25; c >= 0; c--) {
+                    short pos = nextS[L][c];
                     if (pos != -1 && pos <= R) {
                         mid.push_back('a' + c); need = 0; break;
                     }
@@ -45,12 +46,12 @@ string buildPal(string& s,vector<vector<int>>& dp, vector<vector<int>>& nextS,
             break;
         }
         if (min) {
-            for (int c = 0; c < 26; c++) {
-                int i1 = nextS[L][c];
-                int j1 = prevS[R + 1][c];
+            for (short c = 0; c < 26; c++) {
+                short i1 = nextS[L][c];
+                short j1 = prevS[R + 1][c];
                 if (i1 == -1 || j1 == -1) continue;
                 if (i1 >= j1) continue;
-                int inner = (i1 + 1 <= j1 - 1 ? dp[i1 + 1][j1 - 1] : 0);
+                short inner = (i1 + 1 <= j1 - 1 ? dp[i1 + 1][j1 - 1] : 0);
                 if (inner >= need - 2) {
                     leftPart.push_back('a' + c);
                     L = i1 + 1;
@@ -61,12 +62,12 @@ string buildPal(string& s,vector<vector<int>>& dp, vector<vector<int>>& nextS,
                 }
             }
         }else {
-            for (int c = 25; c >= 0; c--) {
-                int i1 = nextS[L][c];
-                int j1 = prevS[R + 1][c];
+            for (short c = 25; c >= 0; c--) {
+                short i1 = nextS[L][c];
+                short j1 = prevS[R + 1][c];
                 if (i1 == -1 || j1 == -1) continue;
                 if (i1 >= j1) continue;
-                int inner = (i1 + 1 <= j1 - 1 ? dp[i1 + 1][j1 - 1] : 0);
+                short inner = (i1 + 1 <= j1 - 1 ? dp[i1 + 1][j1 - 1] : 0);
                 if (inner >= need - 2) {
                     leftPart.push_back('a' + c);
                     L = i1 + 1;
@@ -78,8 +79,8 @@ string buildPal(string& s,vector<vector<int>>& dp, vector<vector<int>>& nextS,
             }
         }
         if (!found) {
-            int valL = (L + 1 <= R ? dp[L + 1][R] : 0);
-            int valR = (L <= R - 1 ? dp[L][R - 1] : 0);
+            short valL = (L + 1 <= R ? dp[L + 1][R] : 0);
+            short valR = (L <= R - 1 ? dp[L][R - 1] : 0);
             if (valL >= valR) L++;
             else R--;
         }
@@ -96,12 +97,12 @@ int main()
 {
     string s;
     cin >> s;
-    int n = s.size();
-    vector<vector<int>> dp(n , vector<int>(n, 0));
-    for (int i = 0; i < n; i++) dp[i][i] = 1;
-    for (int len = 2; len <= n; len++) {
-        for (int l = 0;l + len - 1 < n;l++) {
-            int r = l + len - 1;
+    short n = s.size();
+    vector<vector<short>> dp(n , vector<short>(n, 0));
+    for (short i = 0; i < n; i++) dp[i][i] = 1;
+    for (short len = 2; len <= n; len++) {
+        for (short l = 0;l + len - 1 < n;l++) {
+            short r = l + len - 1;
             if (s[l] == s[r]) {
                 if (l + 1 <= r - 1) dp[l][r] = dp[l + 1][r - 1] + 2;
                 else dp[l][r] = 2;
@@ -112,13 +113,13 @@ int main()
         }
     }
 
-    vector<vector<int>> next(n + 1, vector<int>(26, -1)), prev(n + 1, vector<int>(26, -1));
-    for (int i = n - 1;i >= 0;i--) {
-        for (int c = 0; c < 26; c++) next[i][c] = next[i + 1][c];
+    vector<vector<short>> next(n + 1, vector<short>(26, -1)), prev(n + 1, vector<short>(26, -1));
+    for (short i = n - 1;i >= 0;i--) {
+        for (short c = 0; c < 26; c++) next[i][c] = next[i + 1][c];
         next[i][s[i] - 'a'] = i;
     }
-    for (int i = 0; i < n; i++) {
-        for (int c = 0; c < 26; c++) prev[i + 1][c] = prev[i][c];
+    for (short i = 0; i < n; i++) {
+        for (short c = 0; c < 26; c++) prev[i + 1][c] = prev[i][c];
         prev[i + 1][s[i] - 'a'] = i;
     }
 
